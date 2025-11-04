f8aa45594061957873c157fd922bb1d610f50d84
Adam Vigneaux <adam@blw.ai>
2025-11-04 16:15:21 -0600

    Add test for Templ parsing
    Establishes a baseline for Templ parsing: it works for some rules when
    `templateEngineSyntax` is unset. As we build support for Templ
    parsing, we will be able to enable more rules in this test.

--- packages/eslint-plugin/tests/e2e/index.test.js
+++ packages/eslint-plugin/tests/e2e/index.test.js
@@ -1,6 +1,9 @@
+const path = require(`path`);
 const eslint = require(`eslint`);
 const fs = require(`fs`);
-const path = require(`path`);
+
+const plugin = require(`@html-eslint/eslint-plugin`);
+const parser = require(`@html-eslint/parser`);
 
 const testDirs = fs
   .readdirSync(__dirname, { withFileTypes: true })
@@ -9,41 +12,71 @@ const testDirs = fs
 for (const testDir of testDirs) {
   const testName = testDir.name;
   const testPath = path.join(__dirname, testName);
+  const testConfigPath = path.join(testPath, `test-config.js`);
+  const testConfig = require(testConfigPath);
 
-  test(`e2e/` + testDir.name, () => {
-    const configRules = require(path.join(testPath, `rules.js`));
+  const {
+    sourceFileName,
+    fixedFileName,
+    expectNoMessages = true,
+    expectOutputToMatchSource = true,
+    languageOptions,
+    configOverrides,
+    rules,
+  } = testConfig;
 
+  if (!sourceFileName) {
+    throw new Error(
+      `Missing required "sourceFileName" in ${path.relative(process.cwd(), testConfigPath)}`
+    );
+  }
+
+  test(`e2e/${testName}`, () => {
     const config = {
       plugins: {
-        "@html-eslint": require("@html-eslint/eslint-plugin"),
+        "@html-eslint": plugin,
       },
-      languageOptions: {
-        parser: require(`@html-eslint/parser`),
-      },
-      rules: configRules,
+      languageOptions: Object.assign(
+        {
+          parser,
+        },
+        languageOptions || {}
+      ),
+      rules,
     };
+    if (configOverrides) {
+      Object.assign(config, configOverrides);
+    }
 
     const linter = new eslint.Linter();
+    const sourcePath = path.join(testPath, sourceFileName);
+    const source = fs.readFileSync(sourcePath, { encoding: `utf8` });
 
-    const source = fs.readFileSync(path.join(testPath, `source.html`), {
-      encoding: `utf8`,
-    });
+    const fixedPath =
+      fixedFileName !== undefined ? path.join(testPath, fixedFileName) : null;
+    const hasFixedFile = fixedPath ? fs.existsSync(fixedPath) : false;
 
-    const outputPath = path.join(testPath, `fixed.html`);
-    const shouldHaveErrors = fs.existsSync(outputPath);
-
-    let messages;
-    if (shouldHaveErrors) {
-      const expected = fs.readFileSync(outputPath, { encoding: `utf8` });
-      // @ts-ignore
+    if (hasFixedFile) {
+      if (!fixedPath) {
+        throw new Error(`Expected fixed file path for ${testName}`);
+      }
+      const expected = fs.readFileSync(fixedPath, { encoding: `utf8` });
       const result = linter.verifyAndFix(source, config);
-      messages = result.messages;
+      if (expectNoMessages) {
+        expect(JSON.stringify(result.messages, null, `\t`)).toEqual(`[]`);
+      }
       expect(result.output).toEqual(expected);
-    } else {
-      // If no `fixed.html`, assume the `source.html` should have no errors/warnings
-      // @ts-ignore
-      messages = linter.verify(source, config);
+      return;
     }
-    expect(JSON.stringify(messages, null, `\t`)).toEqual(`[]`); // Quick-and-dirty to check for and print any unfixed errors/warnings
+
+    const messages = linter.verify(source, config);
+    if (expectNoMessages) {
+      expect(JSON.stringify(messages, null, `\t`)).toEqual(`[]`);
+    }
+
+    if (expectOutputToMatchSource) {
+      const fixResult = linter.verifyAndFix(source, config);
+      expect(fixResult.output).toEqual(source);
+    }
   });
 }
---
+++ packages/eslint-plugin/tests/e2e/templ-kitchen-sink/test-config.js
@@ -0,0 +1,32 @@
+const allRules = require("../../../lib//rules/index.js");
+
+const rules = Object.fromEntries(
+  Object.keys(allRules).map((ruleName) => [
+    `@html-eslint/${ruleName}`,
+    "error",
+  ])
+);
+
+module.exports = {
+  sourceFileName: `source.templ`,
+  languageOptions: {
+    parserOptions: {
+      // Parser currently only works when this is unset
+      // templateEngineSyntax: parser.TEMPLATE_ENGINE_SYNTAX.TEMPL,
+    },
+  },
+  rules: {
+    ...rules,
+    "@html-eslint/attrs-newline": "off",
+    "@html-eslint/element-newline": "off",
+    "@html-eslint/id-naming-convention": "off",
+    "@html-eslint/indent": "off",
+    "@html-eslint/lowercase": "off",
+    "@html-eslint/no-duplicate-attrs": "off",
+    "@html-eslint/no-extra-spacing-text": "off",
+    "@html-eslint/quotes": "off",
+    "@html-eslint/require-closing-tags": ["error", {"selfClosing": "always"}],
+    "@html-eslint/require-open-graph-protocol": "off",
+    "@html-eslint/sort-attrs": "off",
+  },
+};
