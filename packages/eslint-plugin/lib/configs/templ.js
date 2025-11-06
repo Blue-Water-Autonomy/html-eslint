05bdcfaf8bdb972862e52bd6625b0f5a86fc375d
Adam Vigneaux <adam@blw.ai>
2025-11-06 09:57:09 -0600

    Disable `quotes` for Templ

--- packages/eslint-plugin/lib/configs/templ.js
+++ packages/eslint-plugin/lib/configs/templ.js
@@ -4,6 +4,7 @@ const templRules = {
   "html/element-newline": "off",
   "html/indent": "off",
   "html/no-duplicate-attrs": "off", // Challenging to validate due to Templ's conditional syntax
+  "html/quotes": "off", // Challenging to validate due to Templ's conditional syntax
 };
 
 module.exports = {
